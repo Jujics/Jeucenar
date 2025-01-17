f53bee5f935467708e6eb0b62875208052eb5a43
Juji.cs <61978075+Jujics@users.noreply.github.com>
2025-01-17 23:45:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const sqlite3 = require('sqlite3').verbose(); 
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
